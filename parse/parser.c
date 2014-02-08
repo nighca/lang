dd2e5927a19a21f46813fc3b1fe793a76291ac57
nighca <nighca@live.cn>
2014-02-08 18:55:04 +0800

    token & syntax parse

---
+++ parse/parser.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "token_parser.h"
+#include "syntax_parser.h"
+
+#define MAX_TOKEN_NUM 100
+
+int resolve(TokenState** tokens, int left, Node* node){
+	if(!left){
+		return 0;
+	}
+
+	TokenState* token;
+	TokenState** originTokens = tokens;
+	int originLeft = left;
+
+	token = *tokens;
+
+	switch(token->type){
+	case TOKEN_STATE_LPT:
+		{
+			Node* n = newNode();
+			tokens++;
+			left--;
+			int offset = 1;
+			int step;
+			while(step = resolve(tokens, left, n)){
+				addChild(node, n);
+				n = newNode();
+				tokens += step;
+				left -= step;
+				offset += step;
+			}
+			token = *tokens;
+			if(token->type == TOKEN_STATE_RPT){
+				return offset + 1;
+			}else{
+				// missing ')'
+				printf("Error: Missing ')'\n");
+				return 0;
+			}
+		}
+		break;
+	case TOKEN_STATE_IDENTIFIER:
+	case TOKEN_STATE_LITERAL:
+	case TOKEN_STATE_OPERATOR:
+		node->val->type = VALUE_TYPE_STRING; 
+		node->val->data.str = token->val;
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, const char * argv[]) {
+	TokenState* result[MAX_TOKEN_NUM];
+	char code[1000] = "";
+	gets(code);
+
+	int num;
+	if(num = doParse(code, result)){
+		Tree* tree = newTree();
+		int resolved = resolve(result, num, tree->root);
+
+		if(!resolved){
+			// no node resolved
+			printf("Error: no node resolved!!!\n");
+		}
+
+		if(resolved < num){
+			// extra token
+			printf("Error: extra token!!!\n");
+		}
+
+		printNode(tree->root, 0);
+	}
+}
